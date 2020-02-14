c19e5c332b42b2b2afff1218e6aef8530a0ef526
Thomas <thomas.curti@epita.fr>
2020-02-14 20:31:50 +0100

    Tests: Add basic Unity test to call the REST API

---
+++ Tests/backend/TestCallToBackend/Assets/BasicCallScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicCallScript : MonoBehaviour
+{
+
+    public InputField inputField;
+    public Text text;
+
+    void Start()
+    {
+        inputField = GetComponentInChildren<InputField>();
+        text = GetComponentInChildren<Text>();
+    }
+
+    public void onClick()
+    {
+        string port = "44314";
+        if (inputField.text != string.Empty)
+        {
+            port = inputField.text;
+        }
+
+        string host = "https://localhost:" + port + "/api/Test";
+
+        text.text = getData(host);
+    }
+
+
+    private string getData(string host)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        return jsonResponse;
+    }
+
+}

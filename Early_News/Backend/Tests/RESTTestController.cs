d8559dcc25fa19ba0b112427c2fb96ea4388e757
Thomas <thomas.curti@epita.fr>
2020-03-03 13:45:07 +0100

    Gitlab: CI: Fix CI

--- Early_News/Backend/Tests/RESTTestController.cs
+++ Early_News/Backend/Tests/RESTTestController.cs
@@ -1,6 +1,7 @@
 using Backend.Controllers;
 using Backend.Dbo;
 using Backend.Model;
+using MySql.Data.MySqlClient;
 using NUnit.Framework;
 using System.Linq;
 
@@ -11,12 +12,39 @@ namespace Tests
 
         AppDb db;
         TestController controller;
+        bool isSetup = false;
 
         [SetUp]
         public void Setup()
         {
             db = new AppDb("server=mariadb;user=root;port=3306;password=admin;database=Test");
             controller = new TestController(db, false);
+
+            if (!isSetup)
+            {
+                isSetup = true;
+                db.Connection.Open();
+                
+                var cmd = db.Connection.CreateCommand();
+                cmd.CommandText = "CREATE TABLE `Test_values` (" +
+	                "`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT," +
+	                "`Value` INT(11) NULL DEFAULT NULL," +
+                    "`Text` LONGTEXT NULL," +
+                    "`Boolean` TINYINT(1) NOT NULL DEFAULT '0'," +
+                    "PRIMARY KEY(`ID`)" +
+                    ");";
+                cmd.ExecuteNonQuery();
+                
+                    
+                MySqlCommand comm = db.Connection.CreateCommand();
+                comm.CommandText = "INSERT INTO Test_values(ID,Value,Text,Boolean) VALUES(@ID, @Value, @Text, @Boolean)";
+                comm.Parameters.AddWithValue("@ID", 1);
+                comm.Parameters.AddWithValue("@Value", 1);
+                comm.Parameters.AddWithValue("@Text", "TestText");
+                comm.Parameters.AddWithValue("@Boolean", 0);
+                comm.ExecuteNonQuery();
+                db.Connection.Close();
+            }
         }
 
         [Test]
@@ -24,7 +52,7 @@ namespace Tests
         {
             var data = controller.Get();
             var test = data.Result.ToList();
-            Assert.AreEqual(test.Count, 3);
+            Assert.AreEqual(test.Count, 1);
         }
 
 

5540c03352dd999a8dce2e3de569f2e765772658
Vincent Masson <vincent1.masson@epita.fr>
2020-03-06 16:08:55 +0100

    SQL: add final tables and update dummydata

--- Early_News/SQL/dbschema.sql
+++ Early_News/SQL/dbschema.sql
@@ -6,11 +6,18 @@ CREATE OR REPLACE TABLE event_type (
     name CHAR(32)
 );
 
+CREATE OR REPLACE TABLE article_source (
+    id   INT      AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(16)
+);
+
 CREATE OR REPLACE TABLE article (
     id           INT       AUTO_INCREMENT PRIMARY KEY,
     title        CHAR(32),
     description  TEXT,
-    full_article LONGTEXT
+    full_article LONGTEXT,
+    source_id    INT,
+    FOREIGN KEY (source_id) REFERENCES article_source (id)
 );
 
 CREATE OR REPLACE TABLE event (
@@ -21,3 +28,15 @@ CREATE OR REPLACE TABLE event (
     FOREIGN KEY (type_id)    REFERENCES event_type (id),
     FOREIGN KEY (article_id) REFERENCES article (id)
 );
+
+CREATE OR REPLACE TABLE dubious_article (
+    id                  INT                    AUTO_INCREMENT PRIMARY KEY,
+    title               CHAR(32),
+    source_id           INT,
+    full_article_source LONGTEXT,
+    other_source_id     INT NULL,
+    full_article_other  LONGTEXT NULL,
+    seen_twice          BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (source_id)       REFERENCES article_source (id),
+    FOREIGN KEY (other_source_id) REFERENCES article_source (id)
+)

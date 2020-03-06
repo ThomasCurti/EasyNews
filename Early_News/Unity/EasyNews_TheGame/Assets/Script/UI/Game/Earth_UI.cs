f0f09b54447bea4ce5d0a9b4e0d969fabaa9d7a3
Jeremie <jeremie2297@gmail.com>
2020-03-06 19:56:13 +0100

    Unity : add Earth map and region selection

---
+++ Early_News/Unity/EasyNews_TheGame/Assets/Script/UI/Game/Earth_UI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth_UI : MonoBehaviour
+{
+    public float rayDistance;
+    public Color[] colors;
+    public string[] texts;
+
+    public Texture2D imageMap;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {   /* Selectione une région */
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, rayDistance))
+        {
+            Renderer renderer = hit.transform.GetComponent<MeshRenderer>();
+            Texture2D texture = renderer.material.mainTexture as Texture2D;
+
+            Vector2 pixelUV = hit.textureCoord;
+            pixelUV.x *= texture.width;
+            pixelUV.y *= texture.height;
+
+            Vector2 tilling = renderer.material.mainTextureScale;
+            Color color = imageMap.GetPixel(Mathf.FloorToInt(pixelUV.x * tilling.x), Mathf.FloorToInt(pixelUV.y * tilling.y));
+
+            int index = FindIndexColor(color);
+            if (index >= 0)
+            {
+                Debug.Log(texts[index]);
+            }
+        }
+    }
+
+    
+    private int FindIndexColor(Color color) 
+    {
+        //Debug.Log(ColorUtility.ToHtmlStringRGB(color));   
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colors[i] == color)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

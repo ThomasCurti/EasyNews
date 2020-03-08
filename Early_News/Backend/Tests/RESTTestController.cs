ea29e50b1e70ef71340541c8badc2d75840c0bb9
Thomas <thomas.curti@epita.fr>
2020-03-08 12:25:16 +0100

    Backend: Add Test

--- Early_News/Backend/Tests/RESTTestController.cs
+++ Early_News/Backend/Tests/RESTTestController.cs
@@ -14,7 +14,7 @@ namespace Tests
 
         AppDb db;
         bool isSetup = false;
-        bool isLocal = false;
+        bool isLocal = true;
 
         [SetUp]
         public void Setup()
@@ -36,24 +36,10 @@ namespace Tests
                 cmd.CommandText = File.ReadAllText(path);
                 cmd.ExecuteNonQuery();
 
-                /*"CREATE TABLE `Test_values` (" +
-                "`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT," +
-                "`Value` INT(11) NULL DEFAULT NULL," +
-                "`Text` LONGTEXT NULL," +
-                "`Boolean` TINYINT(1) NOT NULL DEFAULT '0'," +
-                "PRIMARY KEY(`ID`)" +
-                ");";*/
-
                 MySqlCommand comm = db.Connection.CreateCommand();
                 path = Path.Combine(dir.Parent.Parent.Parent.FullName, "dummydata.sql");
                 comm.CommandText = File.ReadAllText(path);
                 comm.ExecuteNonQuery();
-
-                /*comm.CommandText = "INSERT INTO Test_values(ID,Value,Text,Boolean) VALUES(@ID, @Value, @Text, @Boolean)";
-                comm.Parameters.AddWithValue("@ID", 1);
-                comm.Parameters.AddWithValue("@Value", 1);
-                comm.Parameters.AddWithValue("@Text", "TestText");
-                comm.Parameters.AddWithValue("@Boolean", 0);*/
 
                 db.Connection.Close();
             }
@@ -69,14 +55,90 @@ namespace Tests
             Assert.AreEqual(test.Count, 1);
         }
 
-
         [Test]
-        public void RESTTestGetById()
+        public void RESTArticleGetById()
         {
             var controller = new ArticleController(db, false);
 
             var data = controller.Get(1).Result;
             Assert.AreEqual(data.id, 1);
         }
+
+        [Test]
+        public void RESTArticleSourceGetAllValues()
+        {
+            var controller = new ArticleSourceController(db, false);
+
+            var data = controller.Get();
+            var test = data.Result.ToList();
+            Assert.AreEqual(test.Count, 1);
+        }
+
+        [Test]
+        public void RESTArticleSourceGetById()
+        {
+            var controller = new ArticleSourceController(db, false);
+
+            var data = controller.Get(1).Result;
+            Assert.AreEqual(data.id, 1);
+        }
+
+        [Test]
+        public void RESTDubiousArticleGetAllValues()
+        {
+            var controller = new DubiousArticleController(db, false);
+
+            var data = controller.Get();
+            var test = data.Result;
+            Assert.AreEqual(test, null);
+        }
+
+        [Test]
+        public void RESTDubiousArticleGetById()
+        {
+            var controller = new DubiousArticleController(db, false);
+
+            var data = controller.Get(1).Result;
+            Assert.AreEqual(data, null);
+        }
+
+        [Test]
+        public void RESTEventGetAllValues()
+        {
+            var controller = new EventController(db, false);
+
+            var data = controller.Get();
+            var test = data.Result.ToList();
+            Assert.AreEqual(test.Count, 1);
+        }
+
+        [Test]
+        public void RESTEventGetById()
+        {
+            var controller = new EventController(db, false);
+
+            var data = controller.Get(1).Result;
+            Assert.AreEqual(data.id, 1);
+        }
+
+        [Test]
+        public void RESTEventTypeGetAllValues()
+        {
+            var controller = new EventTypeController(db, false);
+
+            var data = controller.Get();
+            var test = data.Result.ToList();
+            Assert.AreEqual(test.Count, 1);
+        }
+
+        [Test]
+        public void RESTEventTypeGetById()
+        {
+            var controller = new EventTypeController(db, false);
+
+            var data = controller.Get(1).Result;
+            Assert.AreEqual(data.id, 1);
+        }
+
     }
 }

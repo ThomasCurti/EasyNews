9ebe251fd1307d5449e80d7f44f9a31b785aa992
clement.dedenis <clement.dedenis@epita.fr>
2020-03-22 20:57:35 +0100

    Site: added react-router-dom, added routes to components

--- Easy_News/site/src/index.js
+++ Easy_News/site/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Assets/css/index.css';
-import Home from './Components/Home';
 import * as serviceWorker from './serviceWorker';
 
+// Bootstrap
 import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.js';
 
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+// Components
+import Home from "./Components/Home";
+
+// Routes
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+            </Switch>
+        </div>
+    </BrowserRouter>,
+    document.getElementById("app")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

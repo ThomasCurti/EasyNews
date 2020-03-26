a78a3f23bad09eeb4596897d4f77c05f8ac452be
Jeremie <jeremie2297@gmail.com>
2020-03-26 20:42:07 +0100

    Unity: add data json file with its method

--- Easy_News/Unity/EasyNews_TheGame/Assets/Script/monde_data.cs
+++ Easy_News/Unity/EasyNews_TheGame/Assets/Script/monde_data.cs
@@ -1,76 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 public class monde_data : MonoBehaviour
 {
-    public List<Country> coutry_list = new List<Country>();
-
+    public World TheWorld;
 
     
     void Start()
     {
-        /*List<Infrastructure> infra_nunk = new List<Infrastructure>();
-        List<City> city_list = new List<City>();
-        City Nunk = new City(0, "Nunk", 0, 0, 0, 0, false, infra_nunk);
-        Country GroenLand = new Country(coutry_list.Count, "Groenland et Island", city_list);
-        coutry_list.Add(GroenLand);*/
+       getWorldDataFromJson("/WorldData");
     }  
+
+
+    private void getWorldDataFromJson(string file)
+    {
+        string chemin = Application.streamingAssetsPath + file;
+        string jsonString = File.ReadAllText(chemin);
+        TheWorld = JsonUtility.FromJson<World>(jsonString);
+    }
+
+
+}
+[System.Serializable]
+public class World
+{
+    public List<Country> CountryList;
 }
 
+
+[System.Serializable]
 public class Country
 {
-    int id;
-    string name;
-    int climat;
-    List<City> city_list;
-    public Country(int id, string name, List<City> city_list)
-    {
-        this.id = id;
-        this.name = name;
-        this.city_list = city_list;
-    }
+    public int Id;
+    public string Name;
+    public int Climat;
+    public List<City> CityList;
 }
 
-
+[System.Serializable]
 public class City
 {
-    int id;
-    string name;
-    int pop_total;
-    int pop_infecte;
-    int pop_mort;
-    int revenu;
-    bool researchCenter;
-    List<Infrastructure> infra_list;
+    public int Id;
+    public string Name;
+    public int PopTotal;
+    public int PopInfecte;
+    public int PopMort;
+    public int Revenu;
+    public List<Infrastructure> InfraList;
 
-    public City(int id, string name, int pop_total, int pop_infecte, int pop_mort, int revenu, bool researchCenter, List<Infrastructure> infra_list)
-    {
-        this.id = id;
-        this.name = name;
-        this.pop_total = pop_total;
-        this.pop_infecte = pop_infecte;
-        this.pop_mort = pop_mort;
-        this.revenu = revenu;
-        this.researchCenter = researchCenter;
-        this.infra_list = infra_list;
-    }
 }
-
+[System.Serializable]
 public class Infrastructure
 {
-    int id;
-    int typeId;
-    bool construit;
-    bool status;
-    bool researchCenter;
-
-    public Infrastructure(int id, int typeId, bool construit, bool status, bool researchCenter)
-    {
-        this.id = id;
-        this.typeId = typeId;
-        this.construit = construit;
-        this.status = status;
-        this.researchCenter = researchCenter;
-    }
+    public int Id;
+    public int TypeId;
+    public int Status;
 }
+
+/* status :
+ *  0 : non present
+ *  1 : en connstruction
+ *  2 : ouvert
+ *  3 : fermé
+
+ * 
+ * */

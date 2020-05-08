0cdcd89b3ca91832e9fde0440c0abbaa38e57648
Thomas <thomas.curti@epita.fr>
2020-05-08 15:41:54 +0200

    Homepage: Fix style

--- Easy_News/site/src/Components/App.js
+++ Easy_News/site/src/Components/App.js
@@ -43,53 +43,16 @@ class App extends React.Component{
             <div className="App">
                 <BrowserRouter>
                     <Header />
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">
-                                <div className="dropdown">
-                                    <ul className="list-group">
-                                        <li className="list-group-item">
-                                            <button className="btn  dropdown-toggle" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Theme 1
-                                            </button>
-                                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                                <Link className="dropdown-item" to="/article/1">Article 1</Link>
-                                                <Link className="dropdown-item" to="/article/2">Article 2</Link>
-                                                <Link className="dropdown-item" to="/article/3">Article 3</Link>
-                                            </div>
-                                        </li>
-                                        <li className="list-group-item">
-                                            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Theme 2
-                                            </button>
-                                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                                                <Link className="dropdown-item" to="/article/1">Article 1</Link>
-                                                <Link className="dropdown-item" to="/article/2">Article 2</Link>
-                                                <Link className="dropdown-item" to="/article/3">Article 3</Link>
-                                            </div>
-                                        </li>
-                                        <li className="list-group-item"><Link className="dropdown-item" to="/article/1">Article 1</Link></li>
-                                        <li className="list-group-item"><Link className="dropdown-item" to="/article/2">Article 2</Link></li>
-                                        <li className="list-group-item"><Link className="dropdown-item" to="/article/3">Article 3</Link></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-1"/>
-                            <div className="col-8">
+                    {searchText === '' ? (
+                        <Switch>
+                            <Route exact path={"/"} component={Home} />
+                            <Route path={"/list"} component={List_articles} />
+                            <Route path={"/article/:id"} component={ArticleInfo} />
+                        </Switch>
+                    ) : (
+                        <SearchResults />
+                    )}
 
-                                {searchText === '' ? (
-                                    <Switch>
-                                        <Route exact path={"/"} component={Home} />
-                                        <Route path={"/list"} component={List_articles} />
-                                        <Route path={"/article/:id"} component={ArticleInfo} />
-                                    </Switch>
-                                ) : (
-                                    <SearchResults />
-                                )}
-
-                            </div>
-                        </div>
-                    </div>
                     <Footer/>
                 </BrowserRouter>
             </div>
--- Easy_News/site/src/Components/Home.js
+++ Easy_News/site/src/Components/Home.js
@@ -2,14 +2,88 @@ import React from 'react';
 
 // CSS
 import '../Assets/scss/Home.scss';
+import {Link} from "react-router-dom";
 
 function Home() {
     return (
         <div className="Home">
             <div>
-            <h1>HomePage</h1>
-            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                <div className="Home_Title">Vous permet de mieux connaître l'actualité sur les crises mondiales</div>
             </div>
+
+            <div className="homeBlock">
+                <div className="homeBlockLeft">
+                    <div className="homeBlockText">Tenez vous à jour sur les crises</div>
+                    <image className="homeBlockImage"></image>
+                </div>
+                <div className="homeBlockTextLeft">
+                    <div className="homeBlockTextSubtitle">Restez à jour</div>
+                    <div className="homeBlockTextText">Easy News possède un très grand nombre d'article mis à votre disposition. Toutes ces actualités vous permettront de vous tenir à jour sur les grandes crises actuelles. Easy News s'engage à vous fournir un maximum d'article venant de sources sures.</div>
+                    <Link className="homeBlockTextLink" to="/list">En savoir plus ></Link>
+                </div>
+            </div>
+
+            <div className="homeBlock">
+                <div className="homeBlockRight">
+                    <div className="homeBlockText">Apprenez à sauver le monde en jouant</div>
+                    <image className="homeBlockImage"></image>
+                </div>
+                <div className="homeBlockTextRight">
+                    <div className="homeBlockTextSubtitle">Apprenez en jouant</div>
+                    <div className="homeBlockTextText">Easy News est un site mais aussi un jeu ! En jouant avec nous, vous pourrez apprendre en vous amusant les gestes et les actions permettant de sauver le monde ou gérer les grandes crises. Ce jeu est mis à jour régulièrement et utilise les mêmes sources d'informations que pour nos articles.</div>
+                    <Link className="homeBlockTextLink" to="/list">En savoir plus ></Link>
+                </div>
+            </div>
+
+            <div className="line">
+                <div className="circle">
+                    <div className="circleText">
+                        Aidez vos enfants
+                    </div>
+                </div>
+            </div>
+
+            <div className="homeBlock">
+                <div className="homeBlockLeft">
+                    <div className="homeBlockText">Tenez vous à jour sur les crises</div>
+                    <image className="homeBlockImage"></image>
+                </div>
+                <div className="homeBlockTextLeft">
+                    <div className="homeBlockTextSubtitle">Aidez vos enfants à apprendre</div>
+                    <div className="homeBlockTextText">Le jeu Easy News est jouable par les grands mais aussi par les petits ! En jouant à notre jeu, vous permettrez à vos jeunes proches de se renseigner sur les grands problèmes et ainsi de mieux se protéger faces à des pandémies ou face aux désastres naturelles.</div>
+                    <Link className="homeBlockTextLink" to="/list">En savoir plus ></Link>
+                </div>
+            </div>
+
+            <div className="line">
+                <div className="circle">
+                    <div className="circleText">
+                        Qui sommes-nous ?
+                    </div>
+                </div>
+            </div>
+
+            <div className="imagesFooter">
+                <ul>
+                    <li>
+                        <image className="imagesFooterImage"></image>
+                        <div className="imagesFooterText">Clément Dedenis</div>
+                    </li>
+                    <li>
+                        <image className="imagesFooterImage"></image>
+                        <div className="imagesFooterText">Jérémie Piro</div>
+                    </li>
+                    <li>
+                        <image className="imagesFooterImage"></image>
+                        <div className="imagesFooterText">Thomas Curti</div>
+                    </li>
+                    <li>
+                        <image className="imagesFooterImage"></image>
+                        <div className="imagesFooterText">Vincent Masson</div>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     );
 }

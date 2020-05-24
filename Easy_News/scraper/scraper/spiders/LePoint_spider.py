f431880f15b8f4b4ea85428d55cd0175727a4138
clement.dedenis <clement.dedenis@epita.fr>
2020-05-24 17:57:39 +0200

    Scraper: Add LePoint Spider

---
+++ Easy_News/scraper/scraper/spiders/LePoint_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+from ..items import Article
+
+
+def parse_article(response):
+    def extract_with_css(query):
+        return response.css(query).get(default='').strip()
+
+    title = extract_with_css('h1.art-titre::text')
+    description = extract_with_css('h2.art-chapeau::text')
+    full_article = extract_with_css("div.art-text")
+    source = response.url
+    article = Article(title=title, description=description, full_article=full_article, source=source)
+    yield {
+        'Article': article,
+    }
+
+
+class LePointSpider(scrapy.Spider):
+    name = "LePoint"
+    start_urls = [
+        'https://www.lepoint.fr/24h-info/sante/',
+    ]
+
+    def parse(self, response):
+        for articles in response.xpath('//a[has-class("row keep-cols")]'):
+            if articles is not None:
+                yield response.follow(articles, callback=parse_article)
+
+        for pagination_links in response.css('ol.pagination.bottom li'):
+            nextPage = pagination_links.css('a::attr(href)').get()
+            if nextPage is not None:
+                yield response.follow(nextPage, callback=self.parse)

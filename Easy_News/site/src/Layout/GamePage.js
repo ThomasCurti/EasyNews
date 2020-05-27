b054b733f0784f46cccb09fda84b86541ff91a16
Thomas <thomas.curti@epita.fr>
2020-05-27 17:30:41 +0200

    Game: fix game page

--- Easy_News/site/src/Layout/GamePage.js
+++ Easy_News/site/src/Layout/GamePage.js
@@ -2,9 +2,11 @@ import React from "react";
 
 // CSS
 import '../Assets/scss/Home.scss';
+import '../Assets/scss/GamePage.scss';
 
 import HomeBlockLeft from "../Components/HomeBlockLeft";
 import HomeBlockRight from "../Components/HomeBlockRight";
+import {Link} from "react-router-dom";
 
 let sampleText = "Conpingerentur sunt ex suam Asbolius ex ex negotio latius unde vitamque ea petitam ex latius inpetrarunt Sericus suam negotio palaestrita Asbolius autem conpingerentur conpingerentur se adseverantes hi hi autem vitamque negotio praefectum Olybrium Asbolius praefectum unde vincula latius se funditabat tali negotio sunt vitamque suam suam eius et Maxima Campensis petitam.";
 
@@ -12,19 +14,17 @@ const GamePage = () => {
     return (
         <div className="Home">
             <div className="Home_Title">Apprenez en jouant !</div>
-            <div className={"gameDl"}>Télécharger Easy News le jeu</div>
 
-            <HomeBlockLeft
-                title={"Niveau 1"}
-                imageText={"On peut faire ça"}
-                text={sampleText}
-            />
 
-            <HomeBlockRight
-                title={"Niveau 2"}
-                imageText={"Mais aussi ça"}
-                text={sampleText}
-            />
+            <div>
+                <img src="BackgroundAccueil.png" className="gameImage"/>
+                <div className="gameImageContainer">
+                    <Link to="/game">
+                        <h2>Télécharger le jeu en cliquant ici</h2>
+                    </Link>
+                    <p>Jouable par les petits mais aussi par les grands !</p>
+                </div>
+            </div>
 
             <HomeBlockLeft
                 title={"Comment installer"}
@@ -35,6 +35,18 @@ const GamePage = () => {
             <HomeBlockRight
                 title={"Comment jouer"}
                 imageText={"Comment on joue"}
+                text={sampleText}
+            />
+
+            <HomeBlockLeft
+                title={"Niveau 1"}
+                imageText={"On peut faire ça"}
+                text={sampleText}
+            />
+
+            <HomeBlockRight
+                title={"Niveau 2"}
+                imageText={"Mais aussi ça"}
                 text={sampleText}
             />
 
